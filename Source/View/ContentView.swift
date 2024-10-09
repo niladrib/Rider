6dbeafeff9444983c5c57301b46d393e87d92bda
niladrib <niladri.bora@gmail.com>
2024-10-08 21:04:24 -0700

    1. Disable login button when login is in progress

--- Source/View/ContentView.swift
+++ Source/View/ContentView.swift
@@ -13,40 +13,51 @@ struct ContentView: View {
   private let authController = OAuthController()
   private let location = LocationModel()
   @State private var authContext: AuthContext?
+  @State private var isLoginInProgress = false
   
   init(authContext: AuthContext? = nil) {
     self.authContext = authContext
   }
   
   var loggedOutView: some View {
-    Button("Login") {
-      Task {
-//        print("unauthenticated")
-        let webOAuthUrl = URL(string: "https://www.strava.com/oauth/mobile/authorize?client_id=136139&redirect_uri=bora.niladri.Hiker%3A%2F%2Fdevelopers.strava.com&response_type=code&approval_prompt=auto&scope=activity%3Aread%2Cread&state=test")!
-        
-        do {
-          let url = try await authController.webAuth(url: webOAuthUrl, callbackURLScheme: "bora.niladri.Hiker", prefersEphemeralWebBrowserSession: true)
-//          print("callback url=\(url)")
-          let components = URLComponents(string: url.absoluteString)
-          guard let code = components?.queryItems?.filter({ $0.name=="code"}).first?.value else {
-            return
-          }
-          let result = await User.fetchToken(withCode: code)
-          switch result{
-          case let .success(user):
-            self.authContext = AuthContext(isLoggedIn: true, loggedInUser: user)
-            
-          case let.failure(error):
-            print("error=\(error)")
-            self.authContext = AuthContext(isLoggedIn: false)
-          }
-        }catch {
-          print("Got Auth Error=\(error)")
-        }
-        
+    HStack(spacing: 5) {
+      if isLoginInProgress {
+        ProgressView()
       }
+      Button("Login") {
+        isLoginInProgress = true
+        Task {
+          print("unauthenticated")
+          defer {
+            isLoginInProgress = false
+          }
+          let webOAuthUrl = URL(string: "https://www.strava.com/oauth/mobile/authorize?client_id=136139&redirect_uri=bora.niladri.Hiker%3A%2F%2Fdevelopers.strava.com&response_type=code&approval_prompt=auto&scope=activity%3Aread%2Cread&state=test")!
+          
+          do {
+            let url = try await authController.webAuth(url: webOAuthUrl, callbackURLScheme: "bora.niladri.Hiker", prefersEphemeralWebBrowserSession: true)
+            //          print("callback url=\(url)")
+            let components = URLComponents(string: url.absoluteString)
+            guard let code = components?.queryItems?.filter({ $0.name=="code"}).first?.value else {
+              return
+            }
+            let result = await User.fetchToken(withCode: code)
+            switch result{
+            case let .success(user):
+              self.authContext = AuthContext(isLoggedIn: true, loggedInUser: user)
+              
+            case let.failure(error):
+              print("error=\(error)")
+              self.authContext = AuthContext(isLoggedIn: false)
+            }
+          }catch {
+            print("Got Auth Error=\(error)")
+          }
+          
+        }
+      }
+      .buttonStyle(.borderedProminent)
+      .disabled(isLoginInProgress)
     }
-    .buttonStyle(.borderedProminent)
   }
   
   var loggedInView: some View {
